871d8d511c3ed41ab69f51a345b9a2bcbb6e86cc
Moisés Abreu <moises@estudiosite.com.br>
2024-11-14 11:23:27 -0300

    Ajuste layout dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@include('includes.header')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+<!-- Conteúdo Principal -->
+<div class="container main-content py-5">
+    <h2 class="h4 text-dark">
+        {{ __('Admin Dashboard') }}
+    </h2>
+
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-8">
+            <div class="card shadow-sm">
+                <div class="card-body text-center text-muted">
+                    {{ __("Você está logado!") }}
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+
+
+@include('includes.footer')
